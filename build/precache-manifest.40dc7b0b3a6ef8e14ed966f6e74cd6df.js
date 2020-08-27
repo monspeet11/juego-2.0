1ad07542d8b78a54f75473937784c1f3110d3cc2
Esteban Jimenez Ruiz <esteban.jimenez@pragma.com.co>
2020-08-26 23:59:20 -0500

    correcion en la url

---
+++ build/precache-manifest.40dc7b0b3a6ef8e14ed966f6e74cd6df.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88f03e7273c6602be80347702475b80",
+    "url": "/juego-2.0/index.html"
+  },
+  {
+    "revision": "05013330e9b5da498d47",
+    "url": "/juego-2.0/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "988969025e8c9a5ce415",
+    "url": "/juego-2.0/static/js/2.ef941641.chunk.js"
+  },
+  {
+    "revision": "25aea35f7f8b95420058fdbf4abe64bd",
+    "url": "/juego-2.0/static/js/2.ef941641.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05013330e9b5da498d47",
+    "url": "/juego-2.0/static/js/main.7eef4afb.chunk.js"
+  },
+  {
+    "revision": "66f65b3fa23af0449e20",
+    "url": "/juego-2.0/static/js/runtime-main.f947a3fb.js"
+  },
+  {
+    "revision": "10a6cd28270da1cf6fe826da219c4e21",
+    "url": "/juego-2.0/static/media/Kahoot Lobby Music (HD).10a6cd28.mp3"
+  },
+  {
+    "revision": "da62c803e647f8e8a7323c6c715db8ce",
+    "url": "/juego-2.0/static/media/alert_error-01.da62c803.mp3"
+  },
+  {
+    "revision": "cc78ffdc84912894d5812a6b197c88d9",
+    "url": "/juego-2.0/static/media/algae-1.cc78ffdc.svg"
+  },
+  {
+    "revision": "8e4d37b79514cde99c580ccf9533ff53",
+    "url": "/juego-2.0/static/media/algae-2.8e4d37b7.svg"
+  },
+  {
+    "revision": "55d3b0f080f3395416690804b96e016b",
+    "url": "/juego-2.0/static/media/algae-3.55d3b0f0.svg"
+  },
+  {
+    "revision": "e7614693e7c5fb293ac6ecb446de3280",
+    "url": "/juego-2.0/static/media/apple-core.e7614693.svg"
+  },
+  {
+    "revision": "0d4663a1c3a520745f8400cf8b543a33",
+    "url": "/juego-2.0/static/media/bubbles.0d4663a1.svg"
+  },
+  {
+    "revision": "bf7e4509b42d22024146d7a77992890a",
+    "url": "/juego-2.0/static/media/cardboard-box.bf7e4509.svg"
+  },
+  {
+    "revision": "237bbd5644a3ed47070831a128159b83",
+    "url": "/juego-2.0/static/media/coffee-cup2.237bbd56.svg"
+  },
+  {
+    "revision": "0ed319f649449305c0183ccd0fd7bc86",
+    "url": "/juego-2.0/static/media/compostable-bin.0ed319f6.svg"
+  },
+  {
+    "revision": "2c3578ea997eb07cc65700ce564e70d9",
+    "url": "/juego-2.0/static/media/crab.2c3578ea.svg"
+  },
+  {
+    "revision": "71501567f205e9aa3f074f88a67882fe",
+    "url": "/juego-2.0/static/media/dolphin-badge.71501567.svg"
+  },
+  {
+    "revision": "641a386889281de321b380f13338523d",
+    "url": "/juego-2.0/static/media/drinks-can.641a3868.svg"
+  },
+  {
+    "revision": "f6a500f29b6c02f60d0ffab73289c0d3",
+    "url": "/juego-2.0/static/media/earth.f6a500f2.svg"
+  },
+  {
+    "revision": "fff9c7dbe51398beb0bf0de51e093cce",
+    "url": "/juego-2.0/static/media/finger-snap.fff9c7db.mp3"
+  },
+  {
+    "revision": "69bfd90b23553326cf64087825fa0111",
+    "url": "/juego-2.0/static/media/fish-badge.69bfd90b.svg"
+  },
+  {
+    "revision": "cd2a7a1a8710d0f342f75edc98c3f892",
+    "url": "/juego-2.0/static/media/heart-lives.cd2a7a1a.svg"
+  },
+  {
+    "revision": "e46059be08c7417693034e7df1f7b850",
+    "url": "/juego-2.0/static/media/hero_decorative-celebration-02.e46059be.mp3"
+  },
+  {
+    "revision": "eac6d0e27cf3b0fcde00c6217dcebcdf",
+    "url": "/juego-2.0/static/media/jellyfish-smile.eac6d0e2.svg"
+  },
+  {
+    "revision": "8db612d21c045917cdd84db1d78bb364",
+    "url": "/juego-2.0/static/media/milk-carton.8db612d2.svg"
+  },
+  {
+    "revision": "e17db830bdb949b146cf68141d31f95d",
+    "url": "/juego-2.0/static/media/newspaper.e17db830.svg"
+  },
+  {
+    "revision": "a2a5148fb706b0a661bd90c817f92f17",
+    "url": "/juego-2.0/static/media/octopus-smile.a2a5148f.svg"
+  },
+  {
+    "revision": "6c4c92462f09f8532999247584ce342b",
+    "url": "/juego-2.0/static/media/paper.6c4c9246.svg"
+  },
+  {
+    "revision": "966ea1dd36e9fecbf75572b1559ddf99",
+    "url": "/juego-2.0/static/media/pause-icon.966ea1dd.svg"
+  },
+  {
+    "revision": "ef647c41d028a25619105d7844a248bb",
+    "url": "/juego-2.0/static/media/penguin-badge.ef647c41.svg"
+  },
+  {
+    "revision": "a743eea0dfa70c23321327d4767c4f2d",
+    "url": "/juego-2.0/static/media/plastic-bag.a743eea0.svg"
+  },
+  {
+    "revision": "e077ec304d26a70bdac40a5f448a490a",
+    "url": "/juego-2.0/static/media/plastic-bottle.e077ec30.svg"
+  },
+  {
+    "revision": "267ee47ad12da31ff89e4bb1759f6493",
+    "url": "/juego-2.0/static/media/play-icon.267ee47a.svg"
+  },
+  {
+    "revision": "5629667d4f903f1c64e0676023f438bd",
+    "url": "/juego-2.0/static/media/recycle-bin.5629667d.svg"
+  },
+  {
+    "revision": "65b83e892d23cda00862c00a7906f8fc",
+    "url": "/juego-2.0/static/media/red-fish.65b83e89.svg"
+  },
+  {
+    "revision": "2052cd1cd060baf3fbcaf0a008cbc734",
+    "url": "/juego-2.0/static/media/scallop.2052cd1c.svg"
+  },
+  {
+    "revision": "c1ae7be1344e6063c1d5b59674407946",
+    "url": "/juego-2.0/static/media/sea-wave-2.c1ae7be1.svg"
+  },
+  {
+    "revision": "3de31c8febc9ac8e24c41228fac49515",
+    "url": "/juego-2.0/static/media/sea-wave-3.3de31c8f.svg"
+  },
+  {
+    "revision": "a7013303dc68f52a7b6571228b96086f",
+    "url": "/juego-2.0/static/media/sea-wave-4.a7013303.svg"
+  },
+  {
+    "revision": "d092e4548cb47d732605f112d3d9df5f",
+    "url": "/juego-2.0/static/media/sea-wave-5.d092e454.svg"
+  },
+  {
+    "revision": "fc0eafbaaaa03e68259a8fa2d332ed22",
+    "url": "/juego-2.0/static/media/seahorse.fc0eafba.svg"
+  },
+  {
+    "revision": "216e166010b745f026fd756ccf2d18ea",
+    "url": "/juego-2.0/static/media/shirt.216e1660.svg"
+  },
+  {
+    "revision": "0a8b67bd4d9363c24b8f73014d79b386",
+    "url": "/juego-2.0/static/media/space-octopus.0a8b67bd.svg"
+  },
+  {
+    "revision": "72b9485c37613ee2b6b6287c567367bc",
+    "url": "/juego-2.0/static/media/starfish-smile.72b9485c.svg"
+  },
+  {
+    "revision": "424e89166206a20efc40be1acbd8f882",
+    "url": "/juego-2.0/static/media/stars.424e8916.svg"
+  },
+  {
+    "revision": "21060b8b248af4b1aa4edf1f968fcab3",
+    "url": "/juego-2.0/static/media/sweet-wrapper.21060b8b.svg"
+  },
+  {
+    "revision": "1fac14d941fa7d7d831c19e33c1a1fd5",
+    "url": "/juego-2.0/static/media/tap.1fac14d9.svg"
+  },
+  {
+    "revision": "ace4bdccd0e9e9e94ebdeceab9ab3582",
+    "url": "/juego-2.0/static/media/tea-bag.ace4bdcc.svg"
+  },
+  {
+    "revision": "30d2c0302e26ef8cbcf9644a6654bd53",
+    "url": "/juego-2.0/static/media/tin-can.30d2c030.svg"
+  },
+  {
+    "revision": "47706006f74e2706e2aa1ded54724d80",
+    "url": "/juego-2.0/static/media/turtle-badge.47706006.svg"
+  },
+  {
+    "revision": "d10b4237fc2d60e19db0c3d07b8f1932",
+    "url": "/juego-2.0/static/media/tv.d10b4237.svg"
+  },
+  {
+    "revision": "a2816e3211d71e3df805ee87b7fa70da",
+    "url": "/juego-2.0/static/media/walk.a2816e32.svg"
+  },
+  {
+    "revision": "388dc66e00b3d3e14bf2336461564018",
+    "url": "/juego-2.0/static/media/waste-bin-tidyman.388dc66e.svg"
+  },
+  {
+    "revision": "9600d823d291c7af6fbf3247561fe75d",
+    "url": "/juego-2.0/static/media/wine-bottle.9600d823.svg"
+  },
+  {
+    "revision": "4f177631ccd2617100dfb81993938c5f",
+    "url": "/juego-2.0/static/media/x-button.4f177631.svg"
+  }
+]);
